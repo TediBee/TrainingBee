8df8d31062a86070994b26a008629ad9dbe420c9
TediBee <tedi.gunawan@codingbee.org>
2025-01-30 15:19:18 +0700

    tesmodules

---
+++ Titah/maintest.py
@@ -0,0 +1,6 @@
+import modules
+
+print(modules.nama)
+print(modules.list2d)
+a=modules.dictionary["umur"]
+print(a)
---
+++ Titah/modules.py
@@ -0,0 +1,5 @@
+nama="Tester"
+list=[1,2,3,4,5,6,7]
+list2d=[[1,2,3],[4,5,6],[7,8,9]]
+dictionary={'nama':'Tester',"umur":20,'alamat':'Jl. Raya'}
+
